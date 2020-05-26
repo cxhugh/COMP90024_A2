c53f6e6df206f24fe5f977f2e3dd1fae90ad00d3
TriciaOZ <49854978+TriciaOZ@users.noreply.github.com>
2020-05-26 10:49:47 +1000

    Update run-deploy.sh

--- deploy/run-deploy.sh
+++ deploy/run-deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Project: COMP90024--2020S1--Assignment2--Alcohol Tweets and Australian Cities Analytics on the Cloud
+#Purpose:  to find out the correlations between alcohol-related tweets and demographic 
+#         and behavioural characteristics in Australia cities.
+#Team: group05-- Ruiqi Zhu (939162), Zhengyang Li (952972), Jianxin Xu (1014840), Qiuxia YIN (1017231), Fang Qu (1070888)
+
+
 # remove the existed hosts.ini 
 touch ./deploy-couchdb-clusters/inventory/hosts.ini
 rm ./deploy-couchdb-clusters/inventory/hosts.ini
