7cb48466fa56493932fab4b3e2dfe6a1cf21d0e8
TriciaOZ <49854978+TriciaOZ@users.noreply.github.com>
2020-05-26 17:52:01 +1000

    Add files via upload

--- deploy/deploy-couchdb-clusters/run-couchdb.sh
+++ deploy/deploy-couchdb-clusters/run-couchdb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-#Project: COMP90024--2020S1--Assignment2--Alcohol Tweets and Australian Cities Analytics on the Cloud
-#Purpose:  to find out the correlations between alcohol-related tweets and demographic 
-#         and behavioural characteristics in Australia cities.
-#Team: group05-- Ruiqi Zhu (939162), Zhengyang Li (952972), Jianxin Xu (1014840), Qiuxia YIN (1017231), Fang Qu (1070888)
-
+###################################################################################################################################
+##Project: COMP90024--2020S1--Assignment2--Alcohol Tweets and Australian Cities Analytics on the Cloud
+##Purpose:  to find out the correlations between alcohol-related tweets and demographic 
+##          and behavioural characteristics in Australia cities.
+##Team: group05-- Ruiqi Zhu (939162), Zhengyang Li (952972), Jianxin Xu (1014840), Qiuxia YIN (1017231), Fang Qu (1070888)
+###################################################################################################################################
 
 chmod go-wrx ./keys/*.pem
 . ../openrc.sh; ansible-playbook -i ./inventory/hosts.ini -u ubuntu ./couchdb.yml
